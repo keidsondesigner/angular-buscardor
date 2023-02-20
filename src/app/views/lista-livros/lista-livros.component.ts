d993eef962587f5e2efdb6ac970dafcea9ecb8fd
keidsondesigner <keidsondesigner@gmail.com>
2023-02-19 20:20:02 -0400

    tratamento de erro com catchError, thowError

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { filter, map, switchMap, tap } from 'rxjs';
+import { catchError, debounceTime, filter, map, switchMap, tap, throwError } from 'rxjs';
 import { LivrosService } from './../../service/livro.service';
 import { Item, Livro } from 'src/app/models/livros.interface';
 import { LivroVolumeInfo } from 'src/app/models/livro-volume-info.model';
@@ -12,17 +12,27 @@ import { LivroVolumeInfo } from 'src/app/models/livro-volume-info.model';
 })
 export class ListaLivrosComponent {
   campoBusca = new FormControl();
+
   listaLivros: Livro[];
+
+  PAUSA: number = 500;
+
+  mensagemErro: string = '';
 
   constructor(private _serviceLivrosService: LivrosService) { }
 
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
     tap(() => console.log('Fluxo inical')),
+    debounceTime(this.PAUSA),
     filter((valorDigito) => valorDigito.length >= 3),
     switchMap((valorDigitado: string) => this._serviceLivrosService.buscar(valorDigitado)),
     map((items) => this.listaLivros = this.itemsLivros(items)),
+    catchError(erro => {
+      console.log(erro)
+      return throwError(() => new Error(this.mensagemErro = 'Ops, ocorreu um erro. Carregue novamente a página.'))
+    }),
     tap(() => console.log('Requsisições ao servidor'))
-  )
+  );
 
   // utilizando o map() para manipular os criamos uma "Class LivroVolumeInfo" e utilizando o map(),
   // para retornar apenas as informações, que desejamos exibir na tela;
